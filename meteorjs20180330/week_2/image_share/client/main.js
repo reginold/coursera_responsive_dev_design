cf02fb069fb2772612416fbd94e1e7322bf22aaf
reginoldlu <reginold.lu@gmail.com>
2018-04-05 07:18:50 +0900

    modify the files in week2_4

--- meteorjs20180330/week_2/image_share/client/main.js
+++ meteorjs20180330/week_2/image_share/client/main.js
@@ -10,7 +10,9 @@ if (Meteor.isClient){
 
 
   //Template.images.helpers({images:image_data});
-  Template.images.helpers({images:Images.find()});
+  Template.images.helpers({images:
+    Images.find({},{sort:{createdOn:-1,rating:-1}})
+  });
   Template.images.events({
     'click .js-image':function(event){
       console.log(event);
@@ -22,6 +24,28 @@ if (Meteor.isClient){
       $("#"+image_id).hide("slow",function(){
         Images.remove({"_id":image_id});
       })
+    },
+    'click .js-rate-image':function(event){
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
+      Images.update({_id:image_id},
+                    {$set:{rating:rating}});
     }
   });
+  Template.image_add_form.events({
+    'submit .js-add-image':function(event){
+      var img_src,img_alt;
+      img_src = event.target.img_src.value;
+      img_alt = event.target.img_alt.value;
+      console.log("src:"+img_src +"alt:"+img_alt);
+      Images.insert({
+        img_src:img_src,
+        img_alt:img_alt,
+        createdOn:new Date()
+      })
+      return false;
+    }
+  })
 }
