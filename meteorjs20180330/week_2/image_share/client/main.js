73be6afe666b4e265bcdaad94dbfacff8eae39d9
reginoldlu <reginold.lu@gmail.com>
2018-04-04 21:25:53 +0900

    modify the files in week2_3

--- meteorjs20180330/week_2/image_share/client/main.js
+++ meteorjs20180330/week_2/image_share/client/main.js
@@ -7,26 +7,21 @@ console.log(Images.find().count());
 
 if (Meteor.isClient){
   console.log("this is client")
-  var image_data = [
-    {
-      img_src :"laptops.jpg",
-      img_alt : "some laptops"
-    },
-    {
-      img_src :"bass.jpg",
-      img_alt : "bass"
-    },
-    {
-      img_src:"beard.jpg",
-      img_alt:"this is beard"
-    }
-  ];
+
+
   //Template.images.helpers({images:image_data});
   Template.images.helpers({images:Images.find()});
   Template.images.events({
     'click .js-image':function(event){
       console.log(event);
       $(event.target).css('width','50px');
+    },
+    'click .js-del-image':function(event){
+      var image_id = this._id;
+      console.log(image_id);
+      $("#"+image_id).hide("slow",function(){
+        Images.remove({"_id":image_id});
+      })
     }
   });
 }
