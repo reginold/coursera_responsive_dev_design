85e4db0dff42300bf90255c7ec0c791740f4aab9
reginoldlu <reginold.lu@gmail.com>
2018-04-14 18:24:46 +0900

    upload the week3 assignment

---
+++ meteorjs20180330/week_3/week_3_peer_assessment 2/main.js
@@ -0,0 +1,47 @@
+// shared code
+Websites = new Mongo.Collection("websites");
+
+// client only code
+if (Meteor.isClient){
+    // event listeners on the addSiteForm template
+    Template.addSiteForm.events({
+        // this runs when they click the add button... you need to compete it
+     'click .js-add-site':function(event){
+         var url = $('#url_input').val();// get the form value using jquery...
+         var user = "";
+         var user = Meteor.user().emails[0].address;
+         console.log(user);
+        //  if(Meteor.user()){
+        //     var user = Meteor.user().emails[0].address;
+        //     console.log(user);
+        //     return user;
+        //  }
+        //  else{
+        //     console.log("no user");
+        //     return "anonymous person";
+        //  }
+       
+         var site = {"url":url,
+                     "createdOn":new Date(),
+                     "createdBy":user};
+         Websites.insert(site);
+         return false;
+     }
+    });
+
+    // this helper gets the data from the collection for the site-list Template
+    Template.siteList.helpers({
+        'all_websites':function(){
+            return Websites.find({});
+        },
+        'safer_email':function(email){
+            if (email.indexOf('@')!=-1){// we have an email
+                return email.split('@')[0];
+            }
+            else{// probably anonymouse.
+                return email;
+            }
+        }
+    });
+
+}
